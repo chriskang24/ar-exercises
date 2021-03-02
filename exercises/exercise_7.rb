4549c3a074a4b6e73d7f26d4b6fbf941be5c27ad
chriskang24 <christopherjkang24@gmail.com>
2021-03-01 22:09:55 -0500

    complete exercises 1-7 & validation

--- exercises/exercise_7.rb
+++ exercises/exercise_7.rb
@@ -10,3 +10,21 @@ puts "Exercise 7"
 puts "----------"
 
 # Your code goes here ...
+# Exercise 7: Validations for both models
+
+# 2) Ask the user for a store name (store it in a variable)
+
+puts "Please enter a name for the store:" 
+store = gets.chomp 
+
+# 3) Attempt to create a store with the inputted name but leave out the other fields (annual_revenue, mens_apparel, and womens_apparel)
+
+@create_store = Store.create(
+  name: store,
+)
+
+p @create_store
+# 4) Display the error messages provided back from ActiveRecord to the user (one on each line) after you attempt to save/create the record
+
+puts @create_store.valid?
+puts @create_store.errors.full_messages
--- lib/store.rb
+++ lib/store.rb
@@ -1,3 +1,18 @@
+# Stores must always have a name that is a minimum of 3 characters
+# Stores have an annual_revenue that is a number (integer) that must be 0 or more
+# BONUS: Stores must carry at least one of the men's or women's apparel (hint: use a custom validation method - don't use a Validator class)
+
 class Store < ActiveRecord::Base
+  has_many :employees
+  validates :name, length: { minimum: 3 }, presence: true
+  validates :annual_revenue, numericality: { greater_than: 0 }
+  validate :must_have_apparel
+
+  def must_have_apparel
+    if !self.mens_apparel && !self.womens_apparel
+      errors.add(:mens_apparel, "store must have at least one type of apparel")
+      errors.add(:womans_apparel, "store must have at least one type of apparel")
+    end
+  end
 
 end
